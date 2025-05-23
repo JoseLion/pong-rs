f0aaba85ded1fb0ee08f550990a03d6a72479d5d
Jose Luis Leon <joseluis5000l@gmail.com>
2025-05-23 03:51:44 -0500

    feat(core): Scoring (#5)

--- src/game_context.rs
+++ src/game_context.rs
@@ -1,5 +1,5 @@
 use crate::{
-  helpers::shapes::check_collision_circle_rect,
+  helpers::{screen, shapes::check_collision_circle_rect},
   sprites::{
     ball::{Ball, Direction},
     player::{Player, Role},
@@ -22,7 +22,8 @@ impl GameContext {
   }
 
   pub fn update(&mut self) {
-    self.check_collisions();
+    self.handle_collisions();
+    self.handle_score();
     self.move_ai();
 
     self.ball.update();
@@ -37,22 +38,31 @@ impl GameContext {
   }
 
   fn move_ai(&mut self) {
-    let cy = self.ai.y() + (Player::HEIGHT / 2.0);
-
-    if self.ball.y() > cy {
-      self.ai.move_down();
-    } else {
-      self.ai.move_up();
+    match self.ball.y() > self.ai.cy() {
+      true => self.ai.move_down(),
+      false => self.ai.move_up(),
     }
   }
 
-  fn check_collisions(&mut self) {
+  fn handle_collisions(&mut self) {
     if check_collision_circle_rect(self.ball.circle(), self.ai.rect()) {
       self.ball.bounce(Direction::Left);
     }
 
     if check_collision_circle_rect(self.ball.circle(), self.human.rect()) {
       self.ball.bounce(Direction::Right);
+    }
+  }
+
+  fn handle_score(&mut self) {
+    if self.ball.x() - Ball::RADIUS <= 0.0 {
+      self.ai.add_point();
+      self.ball = Ball::new();
+    }
+
+    if self.ball.x() + Ball::RADIUS >= screen::width() {
+      self.human.add_point();
+      self.ball = Ball::new();
     }
   }
 }
